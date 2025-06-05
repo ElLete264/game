b434ea40601df28d73003e78d4b185488c2ba8c2
Carlos Perez Lara <dam24-27@vedrunacentroseducativos.onmicrosoft.com>
2025-06-05 16:16:06 +0200

    Paso 3: añadido atributo 'progreso' e inicializado con guiones

--- src/main/java/Ahorcado/Ahorcado.java
+++ src/main/java/Ahorcado/Ahorcado.java
@@ -2,11 +2,24 @@ package Ahorcado;
 
 public class Ahorcado {
     private String palabraSecreta; // La palabra que hay que adivinar
-
+    private String progreso;       // Lo que el jugador ve ("_ _ _")
     // Constructor: recibe la palabra secreta y la guarda
     public Ahorcado(String palabraSecreta) {
         this.palabraSecreta = palabraSecreta;
-
+        this.progreso = generarProgreso(); // Inicializa con guiones bajos
+    }
+    // Cadena con guiones bajos del mismo tamaño que la palabra secreta
+    private String generarProgreso() {
+        StringBuilder sb = new StringBuilder(); // Crea una cadena vacía para rellenarla
+        for (int i = 0; i < palabraSecreta.length(); i++) {
+            // Recorre cada letra de la palabra secreta (sin mostrarla)
+            sb.append("_"); // Añade un guion bajo por cada letra
+        }
+        return sb.toString(); // Devuelve la cadena completa con guiones ("____")
+    }
+    // Mostrar la palabra
+    public void mostrarPalabra() {
+        System.out.println("Palabra secreta: " + palabraSecreta);
     }
 
 }
