7bed5513a12dc6c7ee1900fb26cf2841cf38a326
Carlos Perez Lara <dam24-27@vedrunacentroseducativos.onmicrosoft.com>
2025-06-05 16:08:02 +0200

    Paso 2: creación constructor Ahorcado (recibe y guarda)

--- src/main/java/Ahorcado/Ahorcado.java
+++ src/main/java/Ahorcado/Ahorcado.java
@@ -1,6 +1,12 @@
 package Ahorcado;
 
 public class Ahorcado {
-    private String palabraSecreta; // Private porque solo se puede usar dentro de esta clase
+    private String palabraSecreta; // La palabra que hay que adivinar
+
+    // Constructor: recibe la palabra secreta y la guarda
+    public Ahorcado(String palabraSecreta) {
+        this.palabraSecreta = palabraSecreta;
+
+    }
 
 }
