8244e9c0b0bdd230e6f2dfd692490c0168d0297b
Carlos Perez Lara <dam24-27@vedrunacentroseducativos.onmicrosoft.com>
2025-06-05 16:22:20 +0200

    Paso 5: añadido método adivinarLetra(char) que actualiza el progreso

--- src/main/java/Ahorcado/Ahorcado.java
+++ src/main/java/Ahorcado/Ahorcado.java
@@ -3,11 +3,13 @@ package Ahorcado;
 public class Ahorcado {
     private String palabraSecreta; // La palabra que hay que adivinar
     private String progreso;       // Lo que el jugador ve ("_ _ _")
+
     // Constructor: recibe la palabra secreta y la guarda
     public Ahorcado(String palabraSecreta) {
         this.palabraSecreta = palabraSecreta;
         this.progreso = generarProgreso(); // Inicializa con guiones bajos
     }
+
     // Cadena con guiones bajos del mismo tamaño que la palabra secreta
     private String generarProgreso() {
         StringBuilder sb = new StringBuilder(); // Crea una cadena vacía para rellenarla
@@ -17,6 +19,7 @@ public class Ahorcado {
         }
         return sb.toString(); // Devuelve la cadena completa con guiones ("____")
     }
+
     // Muestra el progreso actual del jugador
     public void mostrarProgreso() {
         System.out.println("Progreso: " + progreso); // Imprime los guiones
@@ -26,5 +29,16 @@ public class Ahorcado {
         System.out.println("Palabra secreta: " + palabraSecreta);
     }
 
+    // Intenta adivinar una letra
+    public void adivinarLetra(char letra) {
+        StringBuilder nuevoProgreso = new StringBuilder(progreso); // Copia del progreso actual
 
+        for (int i = 0; i < palabraSecreta.length(); i++) {
+            if (palabraSecreta.charAt(i) == letra) {
+                nuevoProgreso.setCharAt(i, letra); // Reemplaza el guion por la letra acertada
+            }
+        }
+        progreso = nuevoProgreso.toString(); // Actualiza el progreso con los cambios
+
+    }
 }
